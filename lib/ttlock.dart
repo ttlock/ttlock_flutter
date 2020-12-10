03fec289d172c1ba1a817df19ce930d3eba504b3
Sciener <513466979@qq.com>
2020-12-10 17:28:05 +0800

    Update ttlock.dart

--- lib/ttlock.dart
+++ lib/ttlock.dart
@@ -103,7 +103,7 @@ class TTLock {
 
   // ignore: slash_for_doc_comments
 /**
-   * Current iPhone/iPoad Bluetooth state
+   * Current Phone/Pad Bluetooth state
    */
   static void getBluetoothState(TTBluetoothStateCallback stateCallback) {
     invoke(COMMAND_GET_BLUETOOTH_STATE, null, stateCallback);
@@ -112,7 +112,7 @@ class TTLock {
 // ignore: slash_for_doc_comments
 /**
  * Initialize the lock
- * map {"lockMac": xxx, "lockVersion": xxx, "isInited": bool}
+ * map {"lockMac": string, "lockVersion": string, "isInited": bool}
  */
   static void initLock(
       Map map, TTLockDataCallback callback, TTFailedCallback failedCallback) {
