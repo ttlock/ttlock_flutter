893018e40a8a86825399fb8846f1be43618a9dde
Sciener <513466979@qq.com>
2021-01-18 11:59:39 +0800

    Update config.dart

--- example/lib/config.dart
+++ example/lib/config.dart
@@ -6,7 +6,9 @@ class Config {
   // custom gateway name
   static String gatewayName = 'My gateway 1';
 
+  // your gateway server ip dress. For example  gatewayServerIp ="192.168.0.0"
   static String gatewayServerIp = '';
 
+  // your gateway server port. For example gatewayServerPort = "8888"
   static String gatewayServerPort = '';
 }
