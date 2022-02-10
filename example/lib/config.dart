cc29e245844bddd6fc200da96340c55a79c96a6d
JLGB <513466979@qq.com>
2022-02-10 09:20:57 +0800

    updateupdate

--- example/lib/config.dart
+++ example/lib/config.dart
@@ -1,9 +1,9 @@
 class Config {
   //ttlock uid,  https://api.ttlock.com/v3/user/getUid
-  static int uid = 1;
+  static int uid = 69;
 
   // custom gateway name
-  static String gatewayName = 'My gateway 1';
+  static String gatewayName = 'Gateway No 1';
 
   // your gateway server ip dress. For example  gatewayServerIp ="121.196.45.100"
   static String gatewayServerIp = '121.196.45.100';
