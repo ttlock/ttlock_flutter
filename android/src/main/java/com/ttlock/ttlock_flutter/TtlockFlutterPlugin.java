023f23fe1efd5183504047867a12518af6e433d1
xtc <tcymac@126.com>
2022-06-29 14:52:54 +0800

    android optimize fingerprint api callback

--- android/src/main/java/com/ttlock/ttlock_flutter/TtlockFlutterPlugin.java
+++ android/src/main/java/com/ttlock/ttlock_flutter/TtlockFlutterPlugin.java
@@ -1086,6 +1086,8 @@ public class TtlockFlutterPlugin implements FlutterPlugin, MethodCallHandler, Ac
         removeCommandTimeOutRunable();
         commandTimeOutCheck();
         ttlockModel.totalCount = totalCount;
+        ttlockModel.currentCount = 0;
+        progressCallbackCommand(commandQue.peek(), ttlockModel.toMap());
       }
 
       @Override
